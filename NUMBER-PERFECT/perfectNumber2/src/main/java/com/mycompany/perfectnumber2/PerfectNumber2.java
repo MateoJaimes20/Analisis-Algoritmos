a233a790cdaed984fb8ba3318b8065d706052303
MateoJaimes20 <majaimesu@ufpso.edu.co>
2024-08-22 15:05:48 -0500

    Perfect Number

---
+++ NUMBER-PERFECT/perfectNumber2/src/main/java/com/mycompany/perfectnumber2/PerfectNumber2.java
@@ -0,0 +1,42 @@
+package com.mycompany.perfectnumber2;
+public class PerfectNumber2 {
+    public static boolean esPerfecto(int numero) {
+
+        int divisoresNumero = 0;
+
+        for (int i = 1; i < numero; i++) {
+
+            if (numero % i == 0) {
+
+                divisoresNumero += i;
+
+            }
+
+        }
+
+        return divisoresNumero == numero;
+    }
+
+    public static void main(String[] args) {
+
+        int numero = 777;
+        long startTime = System.nanoTime();
+
+        if (numero <= 0) {
+
+            System.out.println("Ingres un valor valido");
+
+        }
+
+        if (esPerfecto(numero)) {
+
+            System.out.println("Es un numero perfec mi sog" + numero);
+
+        } else {
+            System.out.println("Lo siento mi sog no es perfect");
+        }
+        long endTime = System.nanoTime();
+        long duration = endTime - startTime;
+        System.out.println("Tiempo de ejecución: " + duration + " nanosegundos");
+    }
+}
---
+++ NUMBER-PERFECT/perfectNumber3/src/main/java/com/mycompany/perfectnumber3/PerfectNumber3.java
@@ -0,0 +1,32 @@
+package com.mycompany.perfectnumber3;
+public class PerfectNumber3 {
+    public static void main(String[] args){
+        System.out.println("Ingrese el número:");
+        int numero = 10;
+        long inicio = System.nanoTime();
+      
+        if (numero <= 0) {
+            System.out.println("El número debe ser positivo.");
+            return;
+        }
+        
+        int sumaDivisores = 0;
+        for (int i = 1; i <= numero /2; i++) {
+            if (numero % i == 0) {
+                sumaDivisores += i;
+            }
+        }
+          
+        if (sumaDivisores == numero) {
+            System.out.println("El número " + numero + " es un número perfecto.");
+        } else {
+            System.out.println("El número " + numero + " no es un número perfecto.");
+        }
+        
+        long fin = System.nanoTime();
+        
+        long tiempo = (fin - inicio);
+  
+        System.out.println("Tiempo de ejecución: " + tiempo + " nanosegundos");
+    }
+}
